9b7618b7d558c30c920f99336e9f92ff781d5e34
VolodymyrBuzinov <64635201+VolodymyrBuzinov@users.noreply.github.com>
2023-04-10 18:14:58 +0300

    add main files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { CurrencyInput } from "components/CurrencyInput/CurrencyInput";
+import "assets/index.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <CurrencyInput />
+    </div>
+  );
+}
+
+export default App;
